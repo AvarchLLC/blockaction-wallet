6c59680e91bc5c88b4fb1454bfa592df1132e9bc
Sanedev <ersandeepranjit@gmail.com>
2017-06-04 16:12:50 +0545

    project initiaition file for ebrwallet

---
+++ frontend/ebrwallet/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { EbrwalletPage } from './app.po';
+
+describe('ebrwallet App', () => {
+  let page: EbrwalletPage;
+
+  beforeEach(() => {
+    page = new EbrwalletPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
