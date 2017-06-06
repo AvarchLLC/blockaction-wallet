a604365e9edfb64510358d8479d9ad7a203c184a
Sanedev <ersandeepranjit@gmail.com>
2017-06-06 17:53:46 +0545

    db config

---
+++ backend/config/database.config.js
@@ -0,0 +1,29 @@
+'use strict';
+function({ {
+    module.exports = {
+        development: {
+            username: 'wallet',
+            password: 'wallet',
+            host: 'localhost',
+            port: '27017',
+            dbName: 'ebrwaller'
+        },
+        production: {
+            username: 'wallet',
+            password: 'wallet',
+            host: 'localhost',
+            port: '27017',
+            dbName: 'ebrwaller'
+        },
+        test: {
+            username: 'xcelTripUser',
+            password: 'xcelTripUser2017#NP',
+            host: 'ds143141.mlab.com',
+            port: '43141',
+            dbName: 'prj_xceltrip'
+
+        }
+    };
+})();
+
+
