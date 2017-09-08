910345a0f6f52f488c8390e8dd5f720ac64f7f2e
Sanedev <ersandeepranjit@gmail.com>
2017-09-08 18:07:10 +0545

    prod env to test network for beta release

--- frontend/ebrwallet/src/environments/environment.prod.ts
+++ frontend/ebrwallet/src/environments/environment.prod.ts
@@ -1,12 +1,19 @@
 export const environment = {
   production: true,
   COIN_API_URL: 'https://api.coinmarketcap.com/v1/ticker',
-  ETH_NODE_URL: 'https://mainnet.infura.io/',
+  ETH_NODE_URL: 'https://kovan.infura.io/',
+  // ETH_NODE_URL: 'https://mainnet.infura.io/',
   API_URL: 'http://web3c.etherworld.co:1234',
   BITCOIN : {
-    TRANSATIONS:     'https://blockexplorer.com/api/addrs/ADDRESS/txs',
-    BALANCE_API:     'https://blockexplorer.com/api/addr/ADDRESS/balance',
-    UTXO_API:        'https://blockexplorer.com/api/addr/ADDRESS/utxo',
-    TRANSACTION_API: 'https://blockexplorer.com/api/tx/send'
+    TRANSATIONS:     'https://testnet.blockexplorer.com/api/addrs/ADDRESS/txs',
+    BALANCE_API:     'https://testnet.blockexplorer.com/api/addr/ADDRESS/balance',
+    UTXO_API:        'https://testnet.blockexplorer.com/api/addr/ADDRESS/utxo',
+    TRANSACTION_API: 'https://testnet.blockexplorer.com/api/tx/send'
   }
+  // BITCOIN : {
+  //   TRANSATIONS:     'https://blockexplorer.com/api/addrs/ADDRESS/txs',
+  //   BALANCE_API:     'https://blockexplorer.com/api/addr/ADDRESS/balance',
+  //   UTXO_API:        'https://blockexplorer.com/api/addr/ADDRESS/utxo',
+  //   TRANSACTION_API: 'https://blockexplorer.com/api/tx/send'
+  // }
 };
